aa9308cc8fcfecc7abef110eb224d704bdf4ddee
lunkant <mordecaiambroise38@gmail.com>
2025-09-15 01:19:40 -0400

    added form functions

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,47 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 const newPostButton = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+const newPostForm = newPostModal.querySelector("#new-post-modal-form");
+const newPostCardImageInput = newPostModal.querySelector("#card-image-input");
+const newPostCardImageCaption = newPostModal.querySelector(
+  "#card-image-caption"
+);
 
 //edit profile
 editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+
   editProfileModal.classList.add("modal_is-opened");
 });
 
 editProfileCloseButton.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  console.log("Submittinf");
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 // New Post
 newPostButton.addEventListener("click", function () {
@@ -25,3 +53,12 @@ newPostButton.addEventListener("click", function () {
 newPostCloseButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  console.log(newPostCardImageInput.value);
+  console.log(newPostCardImageCaption.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
